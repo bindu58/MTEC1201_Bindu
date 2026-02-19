2efc9141e3eed27f0e69d5e1bfe553dae737f45b
bindu58 <bindubiswokarma2001@gmail.com>
2026-02-19 16:01:37 -0500

    Create SS1_Bindu.js

---
+++ SS1_Bindu.js
@@ -0,0 +1,45 @@
+
+/ Bindu_bk//
+//" Perplexed"//
+// Idea of this drawing is that, nowadays we have alot of 
+// choices which make us confused to make decisions. 
+// This drawing depicts the state of perplexed.//
+
+function setup() {
+  createCanvas(900, 800);
+}
+
+function draw() {
+  background(255,100,50);
+
+  fill(0,0,0);
+  rect(0,0,900,550);
+
+ // A man Standing in front of the wall.//
+  ellipse( 450,600,20,30);
+  ellipse(450,650,50,80);
+ rect(455,670,10,70 );
+rect(435,670,10,70);
+
+ // A wall with many choices.//
+  fill( 255,255,255)
+  circle(100, 100, 100);
+    circle(200, 250, 200);
+      circle(500, 300, 300);
+      circle(750,125,200 );
+      circle( 125,450,200);
+      circle(322, 450, 150);
+      circle(300, 100, 150);
+      circle(500, 80, 100);
+      circle(50, 200, 100);
+      circle(750, 400, 200);
+      circle(50, 310, 100);
+      circle(600, 480, 100);
+      circle(850,250,100);
+      circle(700, 250, 50);
+      circle(865, 490, 70);
+
+  fill( 0,255,0)
+ stroke( 0,255,0)
+
+}
