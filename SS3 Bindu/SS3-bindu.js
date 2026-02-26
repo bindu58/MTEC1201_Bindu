d8c3e5315130ea3d6caed9472985195d343795b5
bindu58 <bindubiswokarma2001@gmail.com>
2026-02-26 16:27:42 -0500

    Update SS3-bindu.js

--- SS3 Bindu/SS3-bindu.js
+++ SS3 Bindu/SS3-bindu.js
@@ -1,12 +1,8 @@
 // Bindu//
 
-let r = 100;
-let g = 150;
-let b = 200;
-
-
-
-
+let r = 255;
+let g = 255;
+let b = 255;
 
 function setup() {
   createCanvas(600, 600);
@@ -14,8 +10,40 @@ function setup() {
 }
 
 function draw() {
-  background(50,48,25);
+  background(r,g,b);
+
+  if(mouseX>width/2)
+    {
+      fill(255,0,0);
+      ellipse( width/2, height/2, 100, 100);
+      print("TEST 1 is true");
+      ellipse( width/2, height/2, 50, 50);
+
+0    }
+
+
+
+else if(mouseX<width/2){
+fill(255);
+rect(width/2, height/2, 100, 100);
+print("TEST 2 is true");
+rect(width/2, height/2, 50, 50);
+
 }
 
-function mousePressed() {
+
+else //Test 2 and 2 are false
+{
+  fill(255);
+  rect(width/2, height/2, 100, 100);
+  print("TEST 1 and TEST 2 are false");
+  
+}
+}
+
+function mousePressed() 
+{
+  r = random(255);
+  g = random(100,255);
+  b = random(50,200); 
 }
