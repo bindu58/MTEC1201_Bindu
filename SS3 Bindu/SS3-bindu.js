49202946befaf6556b5fec23c7c16a94228f6a12
bindu58 <bindubiswokarma2001@gmail.com>
2026-02-26 15:48:30 -0500

    new

---
+++ SS3 Bindu/SS3-bindu.js
@@ -0,0 +1,21 @@
+// Bindu//
+
+let r = 100;
+let g = 150;
+let b = 200;
+
+
+
+
+
+function setup() {
+  createCanvas(600, 600);
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(50,48,25);
+}
+
+function mousePressed() {
+}
