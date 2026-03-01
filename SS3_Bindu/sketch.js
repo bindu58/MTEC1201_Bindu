1b6055ee40f7e8c9ef6a5f8dfd38ff4c8f058606
bindu58 <bindubiswokarma2001@gmail.com>
2026-03-01 15:39:44 -0500

    Update sketch.js

--- SS3_Bindu/sketch.js
+++ SS3_Bindu/sketch.js
@@ -3,19 +3,29 @@
 
 
 // " Perplexed"//
-// the picture is all about the state of them perplexed situation because of the too many chooices.//
+// the picture is about the state of perplexity  because of the too many choices.//
+
+let r = 350;
+let str = "FOOL";
 
 
-let x = 300;
-let y = 90;
-let r = 50;
+//ELLIPSE VAEIABLES
+let x = 350;
+let y = 500;
+let dia = 50;
+let isgrowing = true;
+let notGrowing = false;
 
-function setup() {
+function setup() 
+{
   createCanvas(1000,880);
 }
 
 function draw() {
   background(50, 50,255);
+  
+
+  describe('The text "Fool" written in black in the middle of a white rectangle.');
 
   // The line on the left side.//
  stroke(' Black');
@@ -48,31 +58,57 @@ rect(400,833,20,10);
 rect(430,833,20,10);
 
 //circles on the wall//
-strokeWeight(5);
+strokeWeight(0);
 for( let i=0; i<11; i++){
-  fill(255*(i/10),mouseY,);
-  circle(width/1.6, height/3, 300-(i*25),)
+  fill(255*(i/10),25,25,);
+  circle(width/1.65, height/3, r-(i*25),)
 
-
-fill(255,0,0);
-  circle(x, y, r);
-  circle(950, y, r);
-  fill(0,0,0);
-  circle(x, 590, r);
-  circle(950, 590, r);
-
-  y = y + 1;
-  if (y > 600) { 
-    y= 0;
-   
-    }
   }
+  //circle shrinking//
+if (notGrowing){
+  r = r +1; 
 }
+else{  r = r -0.3;
+}
+
+if(r > 250 || r < 50){
+  notGrowing = !!notGrowing;
+}
+
+  
+
+for( let i=0; i<11; i++){
+  fill(150*(i/10),0,0); 
+ ellipse(x, y, dia, dia-(i*10));
+}
+
+for( let i=0; i<11; i++){
+  fill(150*(i/10),0,0); 
+ ellipse(x, y-350, dia, dia-(i*10));
+}
+for( let i=0; i<11; i++){
+  fill(150*(i/10),0,0); 
+ ellipse(x+520, y, dia, dia-(i*10));
+}
+for( let i=0; i<11; i++){
+  fill(150*(i/10),0,0); 
+ ellipse(x+520, y-350, dia, dia-(i*10));
+}
+// the ellipse is growing and shrinking//
+if(isgrowing){
+  dia = dia +2;
+}
+else{
+  dia = dia -3;
+}
+
+if(dia > 250 || dia < 50){
+  isgrowing = !isgrowing;
+}
+
+textSize(50);
+  fill(0,0,0);
+  textSize(14);
+  text(str, 590, 300);
+    }
  
-//stop the circles when they reach the bottom of the wall
- function mousePressed() {
-  noLoop();
-}
-function mouseReleased() {
-  loop();
-}
