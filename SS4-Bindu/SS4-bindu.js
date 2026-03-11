8090709b0048312212c41a21429dd96a69b95a04
bindu58 <bindubiswokarma2001@gmail.com>
2026-03-11 11:56:39 -0400

    4

--- SS4-Bindu/SS4-bindu.js
+++ SS4-Bindu/SS4-bindu.js
@@ -7,6 +7,13 @@
 // Move arrow to x > 700to stop the movement of side circles.//
 // press the mouse to change the color of the wall and the position of the circles.//
 
+// timer//
+currentTime = 0;
+timer1= 2000;
+timer2= 4000;
+timer3= 6000; 
+timer4= 8000;
+
 //create images
 let man;
 let Piano;
@@ -30,25 +37,30 @@ let notGrowing = false;
 function preload()
 {
   wallman = loadImage("Image/wall man.jpg");
+  Piano = loadImage("Image/piano.jpg");
+  trek = loadImage("Image/trek.jpg");
+  man = loadImage("Image/man.jpg");
 }
 
 function setup() 
-{
+ {
   createCanvas(1000,880);
-}
+ }
 
 
-function mousePressed(){
+function mousePressed()
+  {
       g = random(0,255);
       b = random(0,255);
       x = random(400,900);
       y = random(100,600);
       
       
-}
+  }
 
-function draw() {
-  background(50, 50,255);
+function draw() 
+   {
+  background(255);
   
 
   // The line on the left side.//
@@ -59,6 +71,13 @@ function draw() {
 
  // the line on the middle.//
   line( 200,700,1000,700);
+
+
+ //lines around paintings
+ line(23,198,175,198);
+line(23,402,175,402);
+line(23,198,23,402)
+line(176,198,176,402);
 
   // The vertical line//
   line( 200,700,200,0);
@@ -83,37 +102,44 @@ rect(430,833,20,10);
 
 //circles on the wall//
 strokeWeight(0);
-for( let i=0; i<11; i++){
+for( let i=0; i<11; i++)
+  {
   fill(255*(i/10),25,25,);
   circle(width/1.65, height/3, r-(i*25),)
 
   }
   //circle shrinking//
-if (notGrowing){
+if (notGrowing)
+  {
   r = r + 0.5; 
-}
-else{  r = r - 0.2;
-}
+  }
+else
+  {  r = r - 0.2;
+ }
 
-if(r > 250 || r < 50){
+if(r > 250 || r < 50)
+{
   notGrowing = notGrowing;
 }
 
   
 
-for( let i=0; i<11; i++){
+for( let i=0; i<11; i++)
+  {
   fill(150*(i/10),0,0); 
  ellipse(x, y, dia, dia-(i*10));
-}
+  }
 
-for( let i=0; i<11; i++){
+for( let i=0; i<11; i++)
+  {
   fill(150*(i/10),0,0); 
  ellipse(x, y-350, dia, dia-(i*10));
-}
-for( let i=0; i<11; i++){
+  }
+for( let i=0; i<11; i++)
+  {
   fill(150*(i/10),0,0); 
  ellipse(x+520, y, dia, dia-(i*10));
-}
+  }
 for( let i=0; i<11; i++){
   fill(150*(i/10),0,0); 
  ellipse(x+520, y-350, dia, dia-(i*10));
@@ -140,10 +166,64 @@ textSize(50);
   textSize(14);
   text(str, 590, 300);
 
-  stroke(' Black');
-  strokeWeight(4);
-  image(wallman, 25, 200, 150, 200);
+  fill(0,0,0);
+  textStyle(ITALIC);
+  textSize(30);
 
   
+  currentTime = millis();
+  
+
+
+   image(wallman, 25, 200, 150, 200);
+  
+
+   if (currentTime > timer1 && currentTime < timer2)
+   {
+    text('Mistery', 50,435);
+    image(wallman, 25, 200, 150, 200);
+   
+   }
+
+   if (currentTime > timer2 && currentTime < timer3)
+   {
+    image(Piano, 25, 200, 150, 200);
+    text('Music', 50,435);
+   }
+
+   if (currentTime > timer3 && currentTime < timer4)
+   {
+    image(trek, 25, 200, 150, 200);
+    text('Happy', 50,435);
+   }
+   if (currentTime > timer4)
+   {
+    image(man, 25, 200, 150, 200);
+    text('Depth', 50,435);
+   }
+
+}
+
+
+function keyPressed ()
+{
+  if (key === 'W') 
+  {
+    image(wallman, 25, 200, 150, 200);
+    
+  }
+  else if (key === 'P') 
+  {
+    image(Piano, 25, 200, 150, 200);
+  }
+  else if (key === 'T') 
+  {
+    image(trek, 25, 200, 150, 200);
+  }
+   else if (key === 'M') 
+  {
+    image(man, 25, 200, 150, 200);
+  }
+
 }
     
