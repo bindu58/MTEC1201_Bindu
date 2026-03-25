baa4b456fe02b8a97ee509c4a457e2ad89d96b28
bindu58 <bindubiswokarma2001@gmail.com>
2026-03-25 10:24:14 -0400

    s

--- Midterm-Bindu/Midterm-Bindu.js
+++ Midterm-Bindu/Midterm-Bindu.js
@@ -35,7 +35,7 @@ bluebollon;
 let fly = true;
 let pop = false;
 
-let state = "pregame";
+let state;
 
 function preload()
 {
@@ -63,7 +63,7 @@ function draw()
   {
     game();
   }
-  else if (state == "gameover")
+  else if (state == "game over")
   {
     gameOver();
   }
@@ -81,39 +81,13 @@ function draw()
     }
   }
 
- 
-
-
-  
-
-
-
-
 
 function preGame()
 {
-  //background//
-  topcolor = color(0,0,255);
-bottomcolor = color (135,206,235);
- for ( let y = 0; y < height-200; y++) {
-  n = map(y,0,height-200,0,1);
-  let newcolor= lerpColor(topcolor,bottomcolor,n);
-  stroke(newcolor);
-  line(0,y,width,y);
- }
-
- //floor//
- fill(0,100,0);
- rect(0,height-200,width,200);
-
-
-  home(width, height, w, h);
-
+  background(0);
   textSize(50);
-  fill(0);
+  fill(255);
   text("Click To Start The Event", width/2, height/2);
-
-
   }
 
 
@@ -183,24 +157,4 @@ text("Happy New Year!", width/2, height/2);
 
 //function for the home//
 
-function home(width, height, w, h)
- {
- noStroke()
-  fill(0,0,0);
-  rect(width-300, height-340, w+180, h+100);
-  fill(155,70,12);
-  triangle(width-310, height-340, width-60, height-340, width-180, height-450);
-  //door//
-  fill(255,255,255);
-  rect(width-200, height-270, w, h+30);
-  
-  //windows//
-  rect(width-130, height-280, w, h);
-  rect(width-280,height-280,w,h)
-  fill(0)
-  rect(width-258,height-280,w-45,h);
-  rect(width-108,height-280,w-45,h);
-  rect(width-280,height-258,w,h-45);
-  rect(width-130,height-258,w,h-45);
- }
 
