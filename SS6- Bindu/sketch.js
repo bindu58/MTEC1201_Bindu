3fb7d43062642515e5d4e60c850dc18c145be481
bindu58 <bindubiswokarma2001@gmail.com>
2026-04-21 17:51:26 -0400

    ss

--- SS6- Bindu/sketch.js
+++ SS6- Bindu/sketch.js
@@ -1,12 +1,27 @@
 
 
 //Bindu//
-//
+
+
+let y = [];
+let x = [];
+
+let move;
+
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
-function draw() {
-  background(220);
+function draw() 
+{
+  background(0);
+
+
+ for (let i = 0; i < 100; i++) {
+ 
+  circle(width/2, height/2-(i*6), 30,)
+ move  = 3
+ move = move + 0.01
+  }
 }
