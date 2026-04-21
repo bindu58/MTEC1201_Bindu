f4888ce464ded35d79b6d8ef3f007a8fdf565806
bindu58 <bindubiswokarma2001@gmail.com>
2026-04-20 22:00:09 -0400

    ss

--- SS5-BIndu/SS5-Bindu.js
+++ SS5-BIndu/SS5-Bindu.js
@@ -6,7 +6,8 @@
 let r = 120;
 let g = 200;
 let b = 150;
-let d = 200;
+let d = 250;
+let R = 7;
 
 function setup() {
   createCanvas(900, 900,);
@@ -20,19 +21,25 @@ function draw() {
   for (let x = 0; x < width; x += 20) {
     for (let y = 0; y < height; y += 20) {
       fill(r, g, b);
-      circle (x, y, 5);
+      circle (x, y, R);
+      
     }
   }
 
-  let x1 = noise(frameCount * 0.003) * width; 
+  let x1 = noise(frameCount * 0.002) * width; 
    
-  for (let i = 0; i < 35; i++)
+  for (let i = 0; i < 40; i++)
   {  
+    strokeWeight(4);
     fill(r,g,b);
-    square(width/2 ,height/2 , d-(i*20) + x1,30);
+    square(width/2, height/2, d-(i*20) + x1,30);
   }
-
+if (d < 100) {
+  d -= 0.5;
+} else {
+  d = 150;
 }
+  }
 
 
 
