f29f05d05ed50461fbe885e0a5ecec2d56b65483
bindu58 <bindubiswokarma2001@gmail.com>
2026-04-20 20:02:12 -0400

    ss

--- SS5-BIndu/SS5-Bindu.js
+++ SS5-BIndu/SS5-Bindu.js
@@ -6,7 +6,7 @@
 let r = 120;
 let g = 200;
 let b = 150;
-let d = 400;
+let d = 200;
 
 function setup() {
   createCanvas(900, 900,);
@@ -16,17 +16,24 @@ function setup() {
 function draw() {
   background(0);
   
+
+  for (let x = 0; x < width; x += 20) {
+    for (let y = 0; y < height; y += 20) {
+      fill(r, g, b);
+      circle (x, y, 5);
+    }
+  }
+
+  let x1 = noise(frameCount * 0.003) * width; 
+   
   for (let i = 0; i < 35; i++)
-  { 
+  {  
     fill(r,g,b);
-    square(width/2,height/2, d-(i*10),30);
+    square(width/2 ,height/2 , d-(i*20) + x1,30);
   }
 
-  }
+}
 
-  function mouseIsPressed() {
-    d++;
 
-  }
 
 
