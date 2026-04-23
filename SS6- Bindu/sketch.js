3c3b292e075f858c101939852f3bd7769a98cfd8
bindu58 <bindubiswokarma2001@gmail.com>
2026-04-23 14:13:49 -0400

    new

--- SS6- Bindu/sketch.js
+++ SS6- Bindu/sketch.js
@@ -12,7 +12,8 @@ let R = 7;
 
 //
 let c = [25,35,45,55,65,75];
-let a = [];
+let xCoordinates = [];
+let a = [ 0.001, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
 
 function setup() {
   createCanvas(900, 900,);
@@ -33,11 +34,11 @@ function draw() {
 
   let x1 = noise(frameCount * 0.002) * width; 
    
-  for (let i = 0; i < 15; i++)
+  for (let i = 0; i < 50; i++)
   {  
     strokeWeight(4);
     fill(r,g,b, 30);
-    square(width/2, height/2, d-(i*20) + x1,30);
+    square(width/2, height/2, d-(i*10) + x1,30);
   }
 if (d < 50) {
   d -= 0.5;
@@ -47,11 +48,18 @@ if (d < 50) {
 
 
 for (let i = 0; i < 7; i++) {
-  fill(100,150,30,100);
-  circle(100, 100, c[i]);
-  circle (800, 100, c[i]);
-  circle (100, 800, c[i]);
-  circle (800, 800, c[i]);
+  let y1 = noise(frameCount * 0.002) * height;
+  fill(100,150,30,150);
+  circle(50, y1*a[i], c[i]);
+  circle (850, y1*a[i], c[i]);
+  circle (50, y1+a[i], c[i]);
+  circle (850, y1+a[i], c[i]);
+
+
+
+
+
+
   
 
   
