b335d2f95d1e67f9e405f0195197a804a604ea4f
bindu58 <bindubiswokarma2001@gmail.com>
2026-04-28 21:53:47 -0400

    New

---
+++ SS7-Bindu /sketch.js
@@ -0,0 +1,30 @@
+//Bindu//
+
+let tree =[];
+tree.length = 10;
+
+function setup() {
+  createCanvas(800,800);
+}
+
+function draw() {
+  background(220); 
+}
+
+
+
+class Tree
+{
+constructor(TempX,TempY,TempDiameter)
+{
+this.x = TempX;
+this.y = TempY;
+this.diameter = TempDiameter;
+this.grow = [];
+this.grow.length = 10;
+}
+}
+
+
+
+
