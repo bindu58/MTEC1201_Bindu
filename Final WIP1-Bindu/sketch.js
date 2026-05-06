82679543e2326c8595ab2ac33cd5a424d2cf6b01
bindu58 <bindubiswokarma2001@gmail.com>
2026-05-06 13:01:53 -0400

    New

--- Final WIP1-Bindu/sketch.js
+++ Final WIP1-Bindu/sketch.js
@@ -6,7 +6,9 @@ let t = [];
 let str;
 let font;
 let play;
-
+let person;
+let ellipse = []
+let state = "startscreen";
 
 
 function preload()
@@ -14,7 +16,9 @@ function preload()
    font = loadFont('/Images/Easter Joys.otf');
    play = loadImage("Images/play.svg");
 
-}
+  }
+  
+
 
 function setup() {
   createCanvas(1300, 900,WEBGL);
@@ -22,7 +26,17 @@ function setup() {
 }
 
 function draw() {
-startScreen();
+  if (state == "startscreen")
+    {
+      startScreen();
+    }
+    else if ( state == "room")
+    {
+      Room();
+    }
+
+
+  
 }
 
 
@@ -36,11 +50,13 @@ fill(128,0,128);
   fill(128,0,128);
   textSize(100);
   textFont(font);
-  text( str,-350,-250); 
+  text( str,-350,-100); 
   fill(0);
   textSize(60);
   text('PLAY',-100,120);
   image(play,20,50,90,90);
+
+
   
   }
 
@@ -49,7 +65,6 @@ fill(128,0,128);
 
 function Room()
 {
-  translate(0,0,mouseX);
   background(128,0,128);
   x = 0;
   y = 0;
@@ -61,9 +76,41 @@ function Room()
   quad(x-650,y+450,x+650,y+450,x+200, y+200, x-200,y+200);
   fill(128,0,128,);
   rect(x,y,h,h);
- 
+  fill(0);
+  quad(-600,420,-600,0,-450,0,-450,340);
+  quad(-350,285,-350,50,-270,50,-270,240);
+  rect(0,130,70,130);
+  quad(600,420,600,0,450,0,450,340);
+  quad(350,285,350,50,270,50,270,240);
+
   
+  
+
+  
+
 }
+
+class Ellipse
+{
+  constructor( TempX, TempY,TempZ,TempDiameter)
+{
+  this.x = TempX;
+  this.y = TempY;
+  this.z = TempZ;
+  this.diameter = TempDiameter;
+
+
+display()
+{
+  fill(255)
+  circle(this.x,this.y, this.diameter);
+
+}
+}
+}
+
+
+
 
 
 
