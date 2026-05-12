e1991d4bcfe20627aa7468681447049ff642baa9
bindu58 <bindubiswokarma2001@gmail.com>
2026-05-11 20:36:19 -0400

    new

--- Final WIP1-Bindu/sketch.js
+++ Final WIP1-Bindu/sketch.js
@@ -7,26 +7,36 @@ let str;
 let font;
 let play;
 let person;
-let ellipse = []
+let circle = [10,20,30,40,50];
 let state = "startscreen";
 let r = 128;
+let wall;
+let sunrise;
 
 
 function preload()
 {
    font = loadFont('/Images/Easter Joys.otf');
    play = loadImage("Images/play.svg");
-
-  }
+   sunrise = loadImage("Images/Sunrise.png")
+}
   
 
 
-function setup() {
+function setup() 
+{
   createCanvas(1300, 900,WEBGL);
   rectMode(CENTER);
+  for(let i =0; i < 10; i++)
+  {
+    circle[i] = new Circle();
+
+  }
+  wall = new Wall();
 }
 
-function draw() {
+function draw() 
+{
   if (state === "startscreen")
     {
       startScreen();
@@ -39,8 +49,14 @@ function draw() {
     {
       Room2();
     }
-
-
+    else if (state === "room3")
+    {
+      Room3();
+    }
+    else if (state === "lastscreen")
+    {
+      lastScreen();
+    }
   
 }
 
@@ -61,13 +77,13 @@ fill(r,0,128);
   text('PLAY',-100,120);
   image(play,20,50,90,90);
 
-if ( mouseX || mouseY   )
-{
-r = 255;
-}
 
-  
+  if (  -20<mouseX<=20|| 90 <mouseY <=110 )
+  {
+    r = 255;
   }
+
+}
 
 
 
@@ -75,19 +91,12 @@ r = 255;
 function Room()
 {
   background(128,0,128);
-  x = 0;
-  y = 0;
-  h = 400;
- 
-  strokeWeight(5);
-  fill(150,0,150);
-  quad(x-650,y-450,x+650,y-450,x+200,y-200,x-200,y-200);
-  quad(x-650,y+450,x+650,y+450,x+200, y+200, x-200,y+200);
-  fill(128,0,128,);
-  rect(x,y,h,h);
+
+   wall.display();
+   //Doors//
   fill(0);
   quad(-600,420,-600,0,-450,0,-450,340);
-  rect(0,130,70,130);
+  rect(0,135,70,130);
   quad(600,420,600,0,450,0,450,340);
   
 
@@ -96,43 +105,68 @@ function Room()
 
 
 function Room2()
-{background(128,0,128);
-  x = 0;
-  y = 0;
-  h = 400;
- 
-  strokeWeight(5);
-  fill(150,0,150);
-  quad(x-650,y-450,x+650,y-450,x+200,y-200,x-200,y-200);
-  quad(x-650,y+450,x+650,y+450,x+200, y+200, x-200,y+200);
-  fill(128,0,128,);
-  rect(x,y,h,h);
-  fill(0);
+{
+  background(128,0,128);
+
+  wall.display();
+//doors//
+  
+fill(0);
   quad(-600,420,-600,0,-450,0,-450,340);
   quad(-350,285,-350,50,-270,50,-270,240);
-  rect(0,130,70,130);
+  rect(0,135,70,130);
   quad(600,420,600,0,450,0,450,340);
   quad(350,285,350,50,270,50,270,240); 
 
 
 }
 
-class Ellipse
+function Room3 ()
+{
+  background(128,0,128);
+
+  wall.display();
+
+  fill(0);
+  quad(-600,420,-600,0,-450,0,-450,340);
+  quad(-350,285,-350,50,-270,50,-270,240);
+  fill(255,50,25);
+  rect(0,120,70,150,20,20,0,0);
+  fill(0);
+  rect(0,135,70,130,20,20,0,0);
+  
+  
+  quad(600,420,600,0,450,0,450,340);
+  quad(350,285,350,50,270,50,270,240); 
+
+
+
+}
+
+
+
+function lastScreen()
+{
+  background(0);
+  image(sunrise,-650,-450,1300,900);
+}
+
+class Circle
 {
   constructor( TempX, TempY,TempZ,TempDiameter)
-{
+  {
   this.x = TempX;
   this.y = TempY;
   this.z = TempZ;
   this.diameter = TempDiameter;
-}
+  }
 
 display()
-{
-  fill(255)
-  circle(this.x,this.y, this.diameter);
+    {
+      fill(255)
+      circle(this.x,this.y, this.diameter);
 
-}
+    }
 }
 
 
@@ -142,16 +176,55 @@ function mousePressed()
     let mx = mouseX - width/2;
     let my = mouseY - height/2;
 
-  if ( state === "startscreen")
- {
+    if ( state === "startscreen")
+    {
     if (mx > -150 && mx < 150 && my > 25 && my < 175)
-    {state = "room"
+      { 
+      state = "room";
+      }
     }
- }
 
- else if (state === "room")
- {
-  state = "room2";
- }
+        else if (state === "room")
+        {
+        state = "room2";
+        }
+        else if (state === "room2")
+          {
+            state = "room3";
+          }
+
+        else if (state === "room3")
+          {
+            state = "lastscreen";
+          }
+        else if ( state === "lastscreen")
+          {
+          state = "startscreen";
+          }
+
 }
 
+
+
+class Wall
+{
+  constructor()
+  {
+    this.x = 0;
+    this.y = 0;
+    this.h = 400;
+  }
+
+  display()
+  {
+  noStroke();
+  strokeWeight();
+  fill(150,0,150);
+  quad(this.x-650,this.y-450,this.x+650,this.y-450,this.x+200,this.y-200,this.x-200,this.y-200);
+  quad(this.x-650,this.y+450,this.x+650,this.y+450,this.x+200, this.y+200, this.x-200,this.y+200);
+  fill(128,0,128,);
+  rect(this.x,this.y,this.h,this.h);
+
+  }
+
+}
