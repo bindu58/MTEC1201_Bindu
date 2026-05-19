4126e5f05e93983865e7f0ae6f7410ec70c63208
bindu58 <bindubiswokarma2001@gmail.com>
2026-05-19 00:48:07 -0400

    new

--- Final WIP1-Bindu/sketch.js
+++ Final WIP1-Bindu/sketch.js
@@ -9,9 +9,7 @@ let str;
 
 let font;
 let character;
-let character2;
 let person;
-let orbit;
 let state = "startscreen";
 let r = 128;
 
@@ -20,6 +18,14 @@ let sunrise;
 let myCamera;
 let player;
 let circle;
+
+//variable for timer//
+currentTime = 0;
+timer1= 4000;
+timer2= 6000; 
+timer3= 8000;
+timer4= 10000;
+
 
 
 function preload()
@@ -39,9 +45,7 @@ function setup()
   rectMode(CENTER);
   wall = new Wall();
   character = new Character();
-  character2 = new Character(400,300,300,400);
-  circle = new Circle ();
-
+ 
 }
 
 function draw() 
@@ -67,12 +71,17 @@ function draw()
     {
       lastScreen();
     }
+    else if (state === "gameover")
+    {
+      gameOver();
+    }
   
 }
 
 
 function startScreen()
 {
+
 str = 'ESCAPE THE ILLUSION'
 background(0);
 fill(r,0,128);
@@ -87,12 +96,17 @@ fill(r,0,128);
   text('PLAY',-100,120);
   image(play,20,50,90,90);
 
+   let mx = mouseX - width/2;
+    let my = mouseY - height/2;
 
-  if ( 0 < mouseX < 50 && 0 < mouseY < 50)
-  {
-    r = 255;
-  }
-
+    if (mx > -150 && mx < 150 && my > 25 && my < 175)
+    {
+      r = 255;
+    }
+    else
+    {
+    r = 128;
+    }
 }
 
 
@@ -102,13 +116,24 @@ function Room()
 {
   background(0);
    wall.display();
+   
    //Doors//
   fill(0);
-  quad(-600,420,-600,0,-450,0,-450,340);
-  rect(0,135,70,130);
-  quad(600,420,600,0,450,0,450,340);
+  quad(-600,420,-600,0,-450,30,-450,340);
+  rect(0,135,70,130,20,20,0,0);
+  quad(600,420,600,0,450,30,450,340);
   character.display();
   character.update();
+ 
+  currentTime = millis();
+  if ( currentTime == currentTime && currentTime<timer2)
+  {
+     fill(255);
+  textSize(40);
+  text( "To Move The Character Use The Arrow Key",-400,-150);
+  text("Find The Right Door to Escape The Illusion",-400,-90);
+  text ("Find The Right Spot On The Door TO GO Through It",-400,-30);
+  }
 
 }
 
@@ -120,14 +145,15 @@ function Room2()
   wall.display();
 //doors//
   
-fill(0);
-  quad(-600,420,-600,0,-450,0,-450,340);
-  quad(-350,285,-350,50,-270,50,-270,240);
-  rect(0,135,70,130);
-  quad(600,420,600,0,450,0,450,340);
-  quad(350,285,350,50,270,50,270,240);
-  character.display();
+  fill(0);
+  quad(-600,420,-600,0,-450,30,-450,340);
+  quad(-350,285,-350,50,-270,80,-270,240);
+  rect(0,150,70,100,20,20,0,0);
+  quad(600,420,600,0,450,30,450,340);
+  quad(350,285,350,50,270,80,270,240);
+  character.display1();
   character.update();
+    
 
 }
 
@@ -138,14 +164,15 @@ function Room3 ()
   wall.display();
 
   fill(0);
-  quad(-600,420,-600,0,-450,0,-450,340);
-  quad(-350,285,-350,50,-270,50,-270,240);
-  rect(-70,135,70,130,20,20,0,0);
-  rect(70,135,70,130,20,20,0,0);
-  quad(600,420,600,0,450,0,450,340);
-  quad(350,285,350,50,270,50,270,240); 
-  character.display();
+  quad(-600,420,-600,0,-450,30,-450,340);
+  quad(-350,285,-350,50,-270,80,-270,240);
+  rect(-70,150,70,100,20,20,0,0);
+  rect(70,150,70,100,20,20,0,0);
+  quad(600,420,600,0,450,30,450,340);
+  quad(350,285,350,50,270,80,270,240); 
+  character.display2();
   character.update();
+
 
 
 
@@ -158,7 +185,12 @@ function lastScreen()
   orbitControl();
   background(0);
   image(sunrise,-650,-450,1300,900);
-  character2.display();
+  character.display3();
+
+  fill(0);
+  textSize(50);
+  text(" Congratulation! ",-150,-200);
+  text (" You Are Escaped",-180,-140);
 }
 
 
@@ -175,22 +207,14 @@ function mousePressed()
       }
     }
 
-        else if (state === "room")
-        {
-        state = "room2";
-        }
-        else if (state === "room2")
-          {
-            state = "room3";
-          }
-
-        else if (state === "room3")
-          {
-            state = "lastscreen";
-          }
         else if ( state === "lastscreen")
           {
           state = "startscreen";
+          }
+
+          else if (state === "gameover")
+          {
+            state = "room"
           }
 
 }
@@ -214,9 +238,10 @@ class Wall
   fill(150,0,150);
   quad(this.x-650,this.y-450,this.x+650,this.y-450,this.x+200,this.y-200,this.x-200,this.y-200);
   quad(this.x-650,this.y+450,this.x+650,this.y+450,this.x+200, this.y+200, this.x-200,this.y+200);
- 
-  fill(128,0,128);
+  fill(170,0,170);
   rect(this.x,this.y,this.h,this.h);
+  fill(128,0,128);
+ 
   quad(this.x-650,this.y-450,this.x-650,this.y+450,this.x-200,this.y+200,this.x-200,this.y-200);
   quad(this.x+650,this.y-450,this.x+650,this.y+450,this.x+200,this.y+200, this.x+200,this.y-200);
 
@@ -231,8 +256,8 @@ class Character
     this.x=-25;
     this.y=350;
     this.h = 100;
-    this.w = 50
-    this.move = 5;
+    this.w = 50;
+    this.move= 10;
     this.vel = createVector(0,0,0);
   }
 
@@ -253,35 +278,83 @@ class Character
     if (keyIsDown(RIGHT_ARROW))
       this.x ++ === this.move;
 
+    this.x = constrain(this.x, -650,650);
+    this.y = constrain (this.y, 100,350);
+
+
   }
   display()
   {
     image(player,this.x,this.y,this.w,this.h);
 
-    if ( this.y == 100)
+    if ( this.y == 100 && this.x == -25)
     {
-      state = "room2"
+      state = "room2";
+      this.y = 350;
+    }
+    if (this.x == - 550 && this.y<320)
+    {
+      state = "gameover";
       this.y = 350;
     }
 
+    if (this.x == 500 && this.y<320)
+    {
+      state = "gameover";
+      this.t= 350;
+    }
+
+   
+
   }
+  display1()
+  {
+    image(player,this.x,this.y,this.w,this.h);
+
+    if (this.x == - 550 && this.y<320)
+    {
+      state = "room3";
+      this.y = 350;
+      this.x = 0;
+    }
+     if ( this.y == 100 && this.x == -25)
+    {
+      state = "gameover";
+      this.y = 300;
+      this.x = 0;
+    }
+
+    if (this.x == 500 && this.y<320)
+    {
+      state = "gameover";
+      this.t= 300;
+       this.x = 0;
+    }
+
+  }
+display2()
+{
+  image(player,this.x,this.y,this.w,this.h);
+
+    if (this.x == 280 && this.y<190)
+    {
+      state = "lastscreen";
+      this.y = 350;
+    }
+}
+
+display3()
+{
+  image (player,this.x+200,this.y-300,this.w+100,this.h+200);
+}
 
 }
 
-class Circle
+function gameOver()
 {
-  constructor()
-  {
-    this.x = -400;
-    this.y = 100;
-    this.d = 50;
-
-  }
-
-
-  display ()
-    {
-      fill (200,180,0);
-        circle( this.x, this.y, this.d)
-    }
+  wall.display();
+  fill(255,0,0);
+  textSize(70);
+  text("Game Over",-200,-100);
+  text("Click Mouse Restart",-320,-0);
 }
